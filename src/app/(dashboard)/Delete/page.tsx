8ca7a826f2366f9f354d43b5650bca5721324d27
Ayush <ayushtripathi.2104@gmail.com>
2025-12-23 13:48:44 +0530

    analytics

--- src/app/(dashboard)/Delete/page.tsx
+++ src/app/(dashboard)/Delete/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useState } from "react";
 import NewsletterCard from "@/components/inbox/InboxCard"; // ✅ adjust path if needed
 import MobileDeleteSection from "./MobileDeleteSection";
